404ede4af8ced2dc23350f9576467721095bc78f
pangnem <sprnd645@gmail.com>
2022-05-19 09:57:07 +0900

    feat: 웹 개발 기초 - API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,43 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-mvc";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public HelloResponse helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello(name);
+
+        return hello.response();
+    }
+
+    static class Hello {
+        private final String name;
+
+        public Hello(String name) {
+            this.name = name;
+        }
+
+        public HelloResponse response() {
+            return new HelloResponse(this.name);
+        }
+    }
+
+    static class HelloResponse {
+        private final String name;
+
+        public HelloResponse(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 }
