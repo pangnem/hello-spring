7742e1d4f1907ec06cbeeee56ea29747d284d2ee
pangnem <sprnd645@gmail.com>
2022-05-23 10:59:40 +0900

    feat: spring data jpa 추가

--- src/main/java/hello/hellospring/infra/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/infra/SpringDataJpaMemberRepository.java
@@ -1,2 +1,13 @@
-package hello.hellospring.infra;public interface SpringDataJpaMemberRepository {
+package hello.hellospring.infra;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.domain.MemberRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
 }
